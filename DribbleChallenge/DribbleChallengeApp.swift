1a47b2f1298dfa895f67fa80e4cbe376d827f4ea
Adilet <berdibekovadilet@icloud.com>
2021-11-20 16:55:54 +0300

    Initial Commit

---
+++ DribbleChallenge/DribbleChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbleChallengeApp.swift
+//  DribbleChallenge
+//
+//  Created by Adilet Berdibekov on 20.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct DribbleChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
